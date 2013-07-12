93f542af85456a38e790dff6a7f72dfeeb8a2c7b
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-07-11 21:25:50 -0400

    Added tester for exceptions

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 //Check to make sure that numItems returns correct result on a fresh
 // instance of the LinkedList class
-void testCtor(LinkedList<int>& testList){
+void testCtor(){
+  LinkedList<int> testList;
   if(testList.size() == 0){
     std::cout << "SUCCESS: Fresh list has 0 items" << std::endl;
   } else {
@@ -13,7 +14,8 @@ void testCtor(LinkedList<int>& testList){
 
 //Add and remove some items, making sure they come back in the
 // correct order
-void testAddRemove(LinkedList<int>& testList){
+void testBasicMethods(){
+  LinkedList<int> testList;
   testList.add(0,0);
   testList.add(0,1);
   testList.add(0,2);
@@ -36,7 +38,7 @@ void testAddRemove(LinkedList<int>& testList){
   int x5 = testList.get(5);
 
   if(x0 != 2 || x1 != 1 || x2 != 0 || x3 != 3 || x4 != 4 || x5 != 5){
-    std::cout << "ERROR: Expected 2,1,0,3,4,5, but got " << x0 <<"," << x1 << "," << "x2" << "," << "x3" << "," << x4 << "," << x5 << std::endl;
+    std::cout << "ERROR: Expected 2,1,0,3,4,5, but got " << x0 <<"," << x1 << "," << x2 << "," << x3 << "," << x4 << "," << x5 << std::endl;
   } else {
     std::cout << "SUCCESS: 6 added items came back out in the correct order" << std::endl;
   }
@@ -47,7 +49,7 @@ void testAddRemove(LinkedList<int>& testList){
   if(testList.size() == 4){
     std::cout << "SUCCESS: 4 items after removing" << std::endl;
   } else {
-    std::cout << "ERROR: Expected 4 items, but size says " << testList.size() << std::endl;
+    std::cout << "ERROR: After remove, expected 4 items, but size says " << testList.size() << std::endl;
     return;
   }
 
@@ -57,18 +59,113 @@ void testAddRemove(LinkedList<int>& testList){
   x3 = testList.get(3);
 
   if(x0 != 1 || x1 != 0 || x2 != 3 || x3 != 4){
-    std::cout << "ERROR: Expected 1,0,3,4, but got " << x0 <<"," << x1 << "," << "x2" << "," << "x3" << std::endl;
+    std::cout << "ERROR: After remove, expected 1,0,3,4, but got " << x0 <<"," << x1 << "," << x2 << "," << x3 << std::endl;
   } else {
     std::cout << "SUCCESS: After remove, 4 items came back out in the correct order" << std::endl;
   }
 
+  testList.set(0,14);
+  testList.set(1,13);
+  testList.set(2,12);
+  testList.set(3,11);
+  if(testList.size() == 4){
+    std::cout << "SUCCESS: List size not changed after set" << std::endl;
+  } else {
+    std::cout << "ERROR: After set, expected 4 items, but size says " << testList.size() << std::endl;
+    return;
+  }
+
+  x0 = testList.get(0);
+  x1 = testList.get(1);
+  x2 = testList.get(2);
+  x3 = testList.get(3);
+  if(x0 != 14 || x1 != 13 || x2 != 12 || x3 != 11){
+    std::cout << "ERROR: After set, expected 14,13,12,11, but got " << x0 <<"," << x1 << "," << x2 << "," << x3 << std::endl;
+  } else {
+    std::cout << "SUCCESS: After set, 4 items came back out in the correct order" << std::endl;
+  }
+}
+
+void testExceptions(){
+  LinkedList<int> testList;
+  bool didException = false;
+  try {
+    testList.remove(0);
+  } catch (std::string s) {
+    std::cout << "SUCCESS: Caught exception: " << s << std::endl;
+    didException = true;
+  } catch (...) {
+    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+    return;
+  }
+
+  if(!didException){
+    std::cout << "ERROR: Tried to remove from an empty list, but did not get an exception" << std::endl;
+  }
+
+  testList.add(0,0);
+  testList.add(0,1);
+  testList.add(0,2);
+
+  didException = false;
+  try {
+    testList.set(5,5);
+  } catch (std::string s) {
+    std::cout << "SUCCESS: Caught exception: " << s << std::endl;
+    didException = true;
+  } catch (...) {
+    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+    return;
+  }
+
+  if(!didException){
+    std::cout << "ERROR: Tried to set a non-existent item, but did not get an exception" << std::endl;
+  }
+
+  didException = false;
+  try {
+    int t = testList.get(5);
+  } catch (std::string s) {
+    std::cout << "SUCCESS: Caught exception: " << s << std::endl;
+    didException = true;
+  } catch (...) {
+    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+    return;
+  }
+
+  if(!didException){
+    std::cout << "ERROR: Tried to get a non-existent item, but did not get an exception" << std::endl;
+  }
+
+  didException = false;
+  try {
+    testList.add(5,5);
+  } catch (std::string s) {
+    std::cout << "SUCCESS: Caught exception: " << s << std::endl;
+    didException = true;
+  } catch (...) {
+    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+    return;
+  }
+
+  if(!didException){
+    std::cout << "ERROR: Tried to add at invalid index, but did not get an exception" << std::endl;
+  }
+
+
+}
+
+void testSplice(){
+  LinkedList<int> testList1;
+  LinkedList<int> testList2;
 }
 
 //A simple main function which creates a list, and tests it.
 int main(){
-  LinkedList<int> testList;
-  testCtor(testList);
-  testAddRemove(testList);
+  testCtor();
+  testBasicMethods();
+  testExceptions();
+  testSplice();
 
   return 0;
 }
