ec1c1a6b6bc325e6c6af9a3562d05d252fe9422d
Mingwei Zhong <JohnneyMing@users.noreply.github.com>
2013-11-09 19:40:46 -0800

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -18,27 +18,30 @@ class LinkedList : public List <T> {
   virtual unsigned long size();
 
   //Get the value at index i, and return it.
-  // If list does not contain i items, through a string exception
+  // If list does not contain at least i+1 items, throw a string exception
   virtual T get(unsigned long i);
 
   //Set the value at index i to x
-  // If list does not contain i items, through a string exception
+  // If list does not contain at least i+1 items, throw a string exception
   virtual void set(unsigned long i, T x);
 
   //Add a new item, x, at position i. All items that were originally
   // at position i or higher get moved forward 1 to make room.
-  // If list does not contain i items, through a string exception
+  // If list does not contain at least i items, throw a string exception
   virtual void add(unsigned long i, T x);
 
   //Remove the item at position i. All items that were originally
   // at position i+1 or higher get moved backwards 1 to fill the gap.
-  // If list does not contain i items, through a string exception
+  // If list does not contain at least i+1 items, throw a string exception
   virtual void remove(unsigned long i);
 
   //Optional, but may be useful in the Shuffle project
-  //Remove len items, starting with index i, and insert into target list
-  // at position t. 
-  virtual void splice(unsigned long i, unsigned long len, List<T>& target, unsigned long t);
+  //Remove all items from src, and add them to the end of the current list, in the
+  // same order that they started. So if the current list is {4, 1, 2} and src
+  // is {3, 5}, the result should be that the current list is {4, 1, 2, 3, 5} and src is
+  // empty
+  //Note: This should be O(1) time. Use pointer manipulations to graft the lists together.
+  virtual void takeAll(LinkedList<T>& src);
 
   //Initialize all private member variables.
   // Be sure to create the dummy node using "new"
@@ -52,10 +55,10 @@ class LinkedList : public List <T> {
   Node* dummyNode;
 
   //Return a pointer to item i.
-  //Special cases: If i == (unsigned long)-1, return a pointer to the dummyNode
+  //Special cases: If i == numItems, return a pointer to the dummyNode
   //If i is an invalid number, throw a string exception
   Node* find(unsigned long i);
-  
+
   //Number of items in the list
   unsigned long numItems;
 };
