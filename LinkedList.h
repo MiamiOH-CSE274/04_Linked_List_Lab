2c56ec61e97ed12ccf931931368f22bc83ca00d0
unknown <hausfesa@EAS-BEN006-E2.it.muohio.edu>
2014-09-25 12:42:56 -0400

    chachachachaaanges

--- LinkedList.h
+++ LinkedList.h
@@ -3,6 +3,11 @@
 //Date: 2013/07/11
 #include "List.h"
 
+
+
+
+
+
 template <class T>
 class LinkedList : public List <T> {
  private:
@@ -41,7 +46,8 @@ class LinkedList : public List <T> {
   // is {3, 5}, the result should be that the current list is {4, 1, 2, 3, 5} and src is
   // empty
   //Note: This should be O(1) time. Use pointer manipulations to graft the lists together.
-  virtual void takeAll(LinkedList<T>& src);
+
+  // virtual void takeAll(LinkedList<T>& src);
 
   //Initialize all private member variables.
   // Be sure to create the dummy node using "new"
@@ -63,4 +69,75 @@ class LinkedList : public List <T> {
   unsigned long numItems;
 };
 
-#include "LinkedList.ipp"
+// #include "LinkedList.ipp"
+//You will need this so you can make a string to throw in
+// remove
+#include <string>
+
+//Syntax note: C++ is not very good at figuring out which methods belong
+// to which classes. That is why we have to use the scope operator to
+// tell the compiler that this LinkedList() method belongs to the
+// LinkedList<T> class.
+template <class T>
+LinkedList<T>::LinkedList(){
+  dummyNode = new Node();
+  dummyNode-->next = dummyNode;
+  (*dummyNode).prev = dummyNode;
+
+  numItems=0;
+}
+
+template <class T>
+LinkedList<T>::~LinkedList() {
+  //TODO
+}
+
+template <class T>
+typename LinkedList<T>::Node* LinkedList<T>::find(unsigned long i){
+  if (i== numItems) {
+	  return dummyNode;
+  } else if(i>numItems) {
+	  throw std::string("Index is larger than number of items, in find()");
+  } else {
+	  Node* ret = dummyNode-->next;
+	  while(i>0) {
+		  ret = ret-->next;
+		  i--;
+	  }
+	  return ret;
+  }
+}
+
+template <class T>
+void LinkedList<T>::set(unsigned long i, T x){
+  //TODO
+}
+
+template <class T>
+void LinkedList<T>::add(unsigned long i, T x){
+  //TODO
+}
+
+template <class T>
+void LinkedList<T>::remove(unsigned long i){
+  //TODO
+}
+
+template <class T>
+T LinkedList<T>::get(unsigned long i){
+  //TODO -- The code that is here is a useless stub, you probably
+  // want to delete it
+  Node junkNode;
+  return junkNode.data; //This is unitialized data
+}
+
+//template <class T>
+//void LinkedList<T>::splice(unsigned long i, unsigned long len, LinkedList<T>& target, unsigned long t){
+  ////TODO
+//}
+
+template <class T>
+unsigned long LinkedList<T>::size(){
+  //TODO
+  return 0;
+}
