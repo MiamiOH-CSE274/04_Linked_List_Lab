99ba53cebc613b1100e64fa67fccdb40b65b21ef
unknown <MabeCr@EAS-BEN006-B6.it.muohio.edu>
2014-09-25 12:51:02 -0400

    Worked a little on get()

--- LinkedList.h
+++ LinkedList.h
@@ -119,10 +119,12 @@ void LinkedList<T>::remove(unsigned long i){
 
 template <class T>
 T LinkedList<T>::get(unsigned long i){
-	//TODO -- The code that is here is a useless stub, you probably
-	// want to delete it
-	Node junkNode;
-	return junkNode.data; //This is unitialized data
+	Node* junkNode = find(i);
+	if(myNode == dummyNode){
+		throw std::string("In get(), index was too large.");
+	} else {
+		return myNode->data;
+	}
 }
 
 template <class T>
