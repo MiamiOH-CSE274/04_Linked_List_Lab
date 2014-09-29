37120d13970d48c36fcfc4fcc79f77b98e6e0151
Brandon Wilson <wilso199@miamioh.edu>
2014-09-29 19:43:19 -0400

    Finished LinkedList.h, fully successful

--- LinkedList.h
+++ LinkedList.h
@@ -5,53 +5,53 @@
 
 template <class T>
 class LinkedList : public List <T> {
-
+    
 private:
 	struct Node{
 		T data;
 		Node* next;
 		Node* prev;
 	};
-
+    
 public:
 	//See List.h for documentation of these methods
 	//Return the number of items currently in the List
 	virtual unsigned long size();
-
+    
 	//Get the value at index i, and return it.
 	// If list does not contain at least i+1 items, throw a string exception
 	virtual T get(unsigned long i);
-
+    
 	//Set the value at index i to x
 	// If list does not contain at least i+1 items, throw a string exception
 	virtual void set(unsigned long i, T x);
-
+    
 	//Add a new item, x, at position i. All items that were originally
 	// at position i or higher get moved forward 1 to make room.
 	// If list does not contain at least i items, throw a string exception
 	virtual void add(unsigned long i, T x);
-
+    
 	//Remove the item at position i. All items that were originally
 	// at position i+1 or higher get moved backwards 1 to fill the gap.
 	// If list does not contain at least i+1 items, throw a string exception
 	virtual void remove(unsigned long i);
-
+    
 	//Initialize all private member variables.
 	// Be sure to create the dummy node using "new"
 	LinkedList();
 	//Delete any dynamically allocated memory. You will need to loop
 	// through all your nodes, deleting them one at a time
 	virtual ~LinkedList();
-
+    
 private:
 	//Pointer to the dummy node.
 	Node* dummyNode;
-
+    
 	//Return a pointer to item i.
 	//Special cases: If i == numItems, return a pointer to the dummyNode
 	//If i is an invalid number, throw a string exception
 	Node* find(unsigned long i);
-
+    
 	//Number of items in the list
 	unsigned long numItems;
 };
@@ -79,7 +79,7 @@ LinkedList<T>::~LinkedList() {
 
 template <class T>
 typename LinkedList<T>::Node* LinkedList<T>::find(unsigned long i){
-
+    
 	if(i == numItems){
 		return dummyNode;
 	} else if(i > numItems){
@@ -87,7 +87,7 @@ typename LinkedList<T>::Node* LinkedList<T>::find(unsigned long i){
 	} else {
 		Node* ret = dummyNode->next;
 		while(i > 0){
-			ret = ret-> next;
+			ret = ret->next;
 			i--;
 		}
 		return ret;
@@ -96,17 +96,49 @@ typename LinkedList<T>::Node* LinkedList<T>::find(unsigned long i){
 
 template <class T>
 void LinkedList<T>::set(unsigned long i, T x){
-	//TODO
+	
+    if(i + 1 > numItems){
+        throw std::string("Index is too large in set()");
+    } else {
+        Node* targetNode = find(i);
+        targetNode->data = x;
+    }
 }
 
 template <class T>
 void LinkedList<T>::add(unsigned long i, T x){
-	//TODO
+    
+    if(i > numItems){
+        throw std::string("Index is out of bounds, in add()");
+    } else{
+        Node* addIndex = find(i);
+        Node* toAdd = new Node();
+        
+        toAdd->data = x;
+        toAdd->next = addIndex;
+        toAdd->prev = addIndex->prev;
+        toAdd->next->prev = toAdd;
+        toAdd->prev->next = toAdd;
+        
+        numItems++;
+    }
 }
 
 template <class T>
 void LinkedList<T>::remove(unsigned long i){
-	//TODO
+    
+	if(i + 1 > numItems){
+        throw std::string("i + 1 is greater than numItems, in remove()");
+    } else {
+        Node* toDelete = find(i);
+        Node* afterI = toDelete->next;
+        Node* beforeI = toDelete->prev;
+        
+        beforeI->next = afterI;
+        afterI->prev = beforeI;
+        numItems--;
+        delete toDelete;
+    }
 }
 
 template <class T>
@@ -120,14 +152,14 @@ T LinkedList<T>::get(unsigned long i){
 	}
 }
 /*
-template <class T>
-void LinkedList<T>::splice(unsigned long i, unsigned long len, LinkedList<T>& target, unsigned long t){
-//TODO
-}
-*/
+ template <class T>
+ void LinkedList<T>::splice(unsigned long i, unsigned long len, LinkedList<T>& target, unsigned long t){
+ //TODO
+ }
+ */
 template <class T>
 unsigned long LinkedList<T>::size(){
-	//TODO
-	return 0;
+    
+	return numItems;
 }
 
